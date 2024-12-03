f4e8906be1cd612b771c37b702993764a791d1b3
Julia <elnikova_julia@mail.ru>
2024-12-03 23:08:09 +0700

    настраивает роутинг страниц, создает страницу 404

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,18 @@
-const CardQuantity = {
+export const CardQuantity = {
   PlacesCount: 5,
   AllPlaces: 312,
-};
+} as const;
 
-export {CardQuantity};
+export enum RoutePath {
+  Main = '/',
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer/:id',
+  NOT_FAUND = '*',
+}
+
+export enum LoginStatus {
+  Auth = 'AUTH',
+  NotAuth = 'NOT_AUTH',
+  Unknown = 'UNKNOWN',
+}
---
+++ src/pages/no-found/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound(): JSX.Element {
+  return (
+    <div style={{ display: 'block', margin: 'auto', width: 'max-content', textAlign: 'center' }}>
+      <h1>404</h1>
+      <p>Page not found</p>
+      <a href="/">На главную</a>
+    </div>
+  );
+}
