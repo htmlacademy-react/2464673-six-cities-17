83f15ef665ec45640f2d0bc4a40fd1e9bafb4d0f
Julia <elnikova_julia@mail.ru>
2024-12-17 22:00:21 +0700

    работа в adaptors.ts

--- src/adaptors.ts
+++ src/adaptors.ts
@@ -3,6 +3,7 @@ import { CityName, OfferType } from './types';
 type OfferGroups = Record<CityName, OfferType[]>;
 
 export function getOfferGroupes(offers: OfferType[]): OfferGroups {
+  // const result: OfferGroups = Object.groupBy(offers, ({city}) => city.name);
   const result: OfferGroups = {};
 
   offers.forEach((offer) => {
