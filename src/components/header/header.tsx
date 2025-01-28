be1001ce89156617b5634da00fc33385a4f7ddba
Julia <elnikova_julia@mail.ru>
2025-01-28 16:50:38 +0700

    добавляет маршрутизацию

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -11,7 +11,7 @@ export default function Header(): JSX.Element {
       <div className="container">
         <div className="header__wrapper">
           <div className="header__left">
-            <Link className="header__logo-link header__logo-link--active" to='#'>
+            <Link className="header__logo-link header__logo-link--active" to={RoutePath.Main}>
               <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
             </Link>
           </div>
